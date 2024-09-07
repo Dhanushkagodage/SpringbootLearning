40a27a295c27edbdf517a5b5264e27dd69680957
Godagesasindu <godagesasindu@gmail.com>
2024-09-07 23:22:36 +0530

    crud operations

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,18 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class User {
+    @Id
+    private int id;
+    private String name;
 }
